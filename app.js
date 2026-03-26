3f58a35e8707f0557d598d135913ecb9351a903e
arshad nasir <qureshiarshad947@gmail.com>
2026-03-26 22:11:39 +0500

    running route added

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.json());
 
+// ✅ Test route (add this)
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
+
 const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/users', userRoutes);
